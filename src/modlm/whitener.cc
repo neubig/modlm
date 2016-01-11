65844e188d924afb9374979effc8f331dc53a466
Graham Neubig <neubig@gmail.com>
2016-01-11 16:18:47 -0500

    Re-implemented whitener

--- src/modlm/whitener.cc
+++ src/modlm/whitener.cc
@@ -10,18 +10,33 @@ using namespace modlm;
 using namespace Eigen;
 
 // Find the transformation matrix for whitening in Eigen column-major format
-void Whitener::calc_matrix(const AggregateData & data) {
+void Whitener::calc_matrix(const std::vector<std::vector<float> > & data, const std::vector<int> & use_in_training) {
 
   if(type_ == "") return;
+  bool do_mean = (type_ == "mean");
+
+  int data_rows = 0, my_data_row = 0, data_cols = data[0].size();
 
   // Convert the matrix into Eigen
-  int data_rows = data.size();
-  int data_cols = data.data[0].first.first.size();
+  for(int use : use_in_training)
+    if(use > 0)
+      data_rows += (do_mean ? 1 : use);
   MatrixXf X(data_rows, data_cols);
-  for(int i : boost::irange(0, data_rows)) {
-    const auto & out_val = data.data[i];
-    for(int j : boost::irange(0, data_cols))
-      X(i,j) = out_val.first.first[j];
+  int DEBUG_HERE = 0;
+  for(int i : boost::irange(0, (int)data.size())) {
+    const auto & out_val = data[i];
+    int multiplier = use_in_training[i];
+    if(!do_mean) {
+      for(int k = 0; k < multiplier; k++) {
+        for(int j : boost::irange(0, data_cols))
+          X(my_data_row,j) = out_val[j];
+        my_data_row++;
+      }
+    } else if(multiplier != 0) {
+      for(int j : boost::irange(0, data_cols)) 
+        X(my_data_row,j) = out_val[j] * multiplier;
+      my_data_row++;
+    }
   }
   // cerr << "---------- X" << endl << X << endl;
   // Calculate the mean and center
@@ -29,7 +44,7 @@ void Whitener::calc_matrix(const AggregateData & data) {
   Map<RowVectorXf> X_mean(&mean_vec_[0], data_cols);
   X_mean = X.colwise().mean();
 
-  if(type_ == "mean") return;
+  if(do_mean) return;
 
   // Calculate the covariance and perform whitening
   X.rowwise() -= X_mean;
@@ -62,7 +77,7 @@ void Whitener::calc_matrix(const AggregateData & data) {
   }
 }
 // Perform whitening
-void Whitener::whiten(AggregateData & data) {
+void Whitener::whiten(std::vector<std::vector<float> > & data) {
   if(type_ == "") return;
   // Create the data vector
   int data_rows = data.size();
@@ -70,14 +85,14 @@ void Whitener::whiten(AggregateData & data) {
   Map<const RowVectorXf> X_mean(&mean_vec_[0], data_cols);
   // Perform whitening on each example
   if(type_ == "mean") {
-    for(int i : boost::irange(0, data_rows)) {
-      Map<RowVectorXf> x(&data.data[i].first.first[0], data_cols);
+    for(auto & my_data : data) {
+      Map<RowVectorXf> x(&my_data[0], data_cols);
       x = (x - X_mean);
     }
   } else {
     Map<const MatrixXf> W(&rotation_vec_[0], data_cols, data_cols);
-    for(int i : boost::irange(0, data_rows)) {
-      Map<RowVectorXf> x(&data.data[i].first.first[0], data_cols);
+    for(auto & my_data : data) {
+      Map<RowVectorXf> x(&my_data[0], data_cols);
       x = (x - X_mean) * W;
     }
   }
